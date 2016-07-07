c00df5ea118c60b7869f82c59b08eb6b44be1c2a
hzruandd <82433422@qq.com>
2016-07-07 09:50:39 +0800

    add iptables.sh

--- monitor/iptables.sh
+++ monitor/iptables.sh
@@ -1,34 +1,24 @@
-#Traffic to and from mongod Instances
-iptables -A INPUT -s <ip-address> -p tcp --destination-port 27017 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -d <ip-address> -p tcp --source-port 27017 -m state --state ESTABLISHED -j ACCEPT
-ip-address such as: 180.97.33.108,10.10.10.10/24,10.10.10.10/255.255.255.0
+#配置mongod Instances
+    iptables -A INPUT -s <ip-address> -p tcp --destination-port 27017 -m state --state NEW,ESTABLISHED -j ACCEPT
+    iptables -A OUTPUT -d <ip-address> -p tcp --source-port 27017 -m state --state ESTABLISHED -j ACCEPT
+    ip-address such as: 180.97.33.108,10.10.10.10/24,10.10.10.10/255.255.255.0
 
-#Traffic to and from mongos Instances
-#the same to <Traffic to and from mongod Instances>
+#配置mongos Instances
+#此处配置同上mongod Instances
 
 
-#Traffic to and from a MongoDB Config Server
+#配置MongoDB Config Server
 iptables -A INPUT -s <ip-address> -p tcp --destination-port 27019 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -d <ip-address> -p tcp --source-port 27019 -m state --state ESTABLISHED -j ACCEPT
 
-#Additionally, config servers need to allow incoming connections from all of
-#the mongos instances in the cluster and all mongod instances in the cluster.
-#Add rules that resemble the following
+#因为所有mongos需要连接到config服务器，而config和mongod也需要连接，故：
 iptables -A INPUT -s <ip-address> -p tcp --destination-port 27019 -m state --state NEW,ESTABLISHED -j ACCEPT
 
-#Traffic to and from a MongoDB Shard Server
+#配置MongoDB Shard Server
 iptables -A INPUT -s <ip-address> -p tcp --destination-port 27018 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -d <ip-address> -p tcp --source-port 27018 -m state --state ESTABLISHED -j ACCEPT
-#This allows you to permit incoming and outgoing traffic between all shards
-#including constituent replica set members, to:
-#all mongod instances in the shard’s replica sets.
-#all mongod instances in other shards
 
-#Provide Access For Monitoring Systems
-#The mongostat diagnostic tool, when running with the --discover needs to be
-#able to reach all components of a cluster, including the config servers, the
-#shard servers, and the mongos instances.
-#
+#此处配置准许MongoDB的Monitoring Systems
 iptables -A INPUT -s <ip-address> -p tcp --destination-port 28017 -m state --state NEW,ESTABLISHED -j ACCEPT
 
 
@@ -38,7 +28,7 @@ iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 
 
-#Make all iptables Rules Persistent
+#由于iptables的配置是保留在内存中，服务器关机后就丢失了，故需要落地：
 #Red Hat Enterprise Linux, Fedora Linux
 service iptables save
 
